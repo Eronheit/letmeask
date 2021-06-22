a60c29298af51de595215af8d73af2157dc9094b
Eronheit <aquinoeronaldo@gmail.com>
2021-06-22 12:22:09 -0300

    chore: firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './services/firebase';
 
 ReactDOM.render(
   <React.StrictMode>
