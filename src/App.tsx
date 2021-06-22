3109ec8d2fc096a4a1aba7ec11cd8495cac58634
Eronheit <aquinoeronaldo@gmail.com>
2021-06-22 20:35:18 -0300

    feat: add auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Routes from './routes';
 
+import { AuthContextProvider } from './contexts/AuthContext'
+
 function App() {
   return (
+    <AuthContextProvider>
       <Routes />
+    </AuthContextProvider>
   );
 }
 
