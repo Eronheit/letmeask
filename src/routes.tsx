6e4b5f43a311a0bf978cc305e5ff712027ef08ec
Eronheit <aquinoeronaldo@gmail.com>
2021-06-24 23:00:55 -0300

    feat: add page Room

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,17 @@
-import { Route,  BrowserRouter } from 'react-router-dom'
+import { Route,  BrowserRouter, Switch } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new"exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
+      </Switch>
     </BrowserRouter>
   );
 }
