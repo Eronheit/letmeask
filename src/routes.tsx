d1579436148d3f1916c270af1a6d7369ec799de8
Eronheit <aquinoeronaldo@gmail.com>
2021-06-24 23:24:37 -0300

    feat: add page AdminRoom

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Route,  BrowserRouter, Switch } from 'react-router-dom'
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from './pages/AdminRoom';
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new"exact component={NewRoom} />
         <Route path="/rooms/:id" component={Room} />
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
       </Switch>
     </BrowserRouter>
   );
